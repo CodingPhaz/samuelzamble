a44e1382aa4d2cf26ff19b2cd38f52a71092569f
Sophonie Bouye <bouyesophonie@gmail.com>
2022-12-21 16:03:07 +0000

    change function Rectangle to class

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 
 // Rectangle constructor
-function Rectangle (x, y, width, height, color){
-	this.x = x	
-	this.y = y	
-	this.width = width
-	this.height = height	
-	this.color = color
-	let id = 1
+class Rectangle {
+	static idInc = 0
+	id;
 
-	this.draw = function () {
+	constructor(x, y, width, height, color) {
+		this.x = x	
+		this.y = y	
+		this.width = width
+		this.height = height	
+		this.color = color
+	}
+
+	draw = function () {
 		context.strokeStyle = this.color
 		context.strokeRect(this.x, this.y, this.width, this.height)
-		id += 1
-	} 
+		this.id = ++Rectangle.idInc
+
+		return {
+			id : this.id,
+			context: context
+		}
+	}
 }
 
 const width = canvas.width
@@ -25,8 +34,15 @@ const boardSize = width / boardLength
 for(x = 0; x < width; x += boardSize){
 	for(y = 0; y < width; y+= boardSize){
 		let rectangle = new Rectangle(x, y, boardSize, boardSize, 'black')
-		rectangle.draw()
-		console.log(rectangle.id)
+		const draw = rectangle.draw()
+		drawMark(draw)
 	}
 }
 
+
+function drawMark(draw) {
+	addEventListener("click", (e) => {
+		console.log(draw)
+
+	})
+}
